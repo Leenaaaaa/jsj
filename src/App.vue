c96ae04019c43e679b3a6d2b835d97852f81356a
leena <623360853@qq.com>
2018-10-18 19:22:08 +0800

    项目第一天 已经完成了轮播图的数据和六宫格和导航栏的动画

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,23 @@
   <div id="app" class="app-container">
    <mt-header fixed title="黑马程序员·Vue项目"></mt-header>
     
-    
-    <router-view/>
+    <transition>
+     <router-view/>
+    </transition>
+   
 
      <!-- 底部 Tabbar 区域 -->
     <nav class="mui-bar mui-bar-tab">
 			<router-link class="mui-tab-item" to="/home">
 				<span class="mui-icon mui-icon-home"></span>
 				<span class="mui-tab-label">首页</span>
+
 			</router-link>
 			<router-link class="mui-tab-item" to="/member">
 				<span class="mui-icon mui-icon-contact"></span>
 				<span class="mui-tab-label">会员</span>
 			</router-link>
+
 			<router-link class="mui-tab-item" to="/shopcar">
 				<span class="mui-icon mui-icon-extra mui-icon-extra-cart">
 					<span class="mui-badge">0</span>
@@ -26,6 +30,7 @@
 				<span class="mui-tab-label">搜索</span>
 			</router-link>
 		</nav>
+
 
   </div>
 </template>
@@ -40,7 +45,26 @@ export default {
 <style scoped>
 .app-container {
     padding-top: 40px;
+    overflow-x: hidden;
+    
   
+}
+.v-enter{
+  opacity: 0;
+  transform: translateX(100%);
+ 
+
+}
+.v-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+  position: absolute
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease;
+
 }
 
 
