464a74d04e233913c010fa21063abdf133adb1ef
leena <623360853@qq.com>
2018-10-18 12:36:45 +0800

    头部t底部

--- src/components/XiangYang.vue
+++ src/components/XiangYang.vue
@@ -1,7 +1,33 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <router-view>1</router-view>
+  <div class="app-container">
+    <mt-header fixed></mt-header>
+    <router-view>
+        11111111111
+    
+    
+    
+    </router-view>
+      <!-- 底部 Tabbar 区域 -->
+    <nav class="mui-bar mui-bar-tab">
+			<router-link class="mui-tab-item" to="/home">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/member">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">会员</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/shopcar">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+					<span class="mui-badge">0</span>
+				</span>
+				<span class="mui-tab-label">购物车</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/search">
+				<span class="mui-icon mui-icon-search"></span>
+				<span class="mui-tab-label">搜索</span>
+			</router-link>
+		</nav>
   </div>
 </template>
 
